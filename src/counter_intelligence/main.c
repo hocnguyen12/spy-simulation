e7b4f33a2c5ad3f04dc4dc02470e4504bac61d43
root <root@Imothep.localdomain>
2024-01-06 21:00:34 +0100

    COunter_intellignce

---
+++ src/counter_intelligence/main.c
@@ -0,0 +1,43 @@
+/*
+ 
+The License to Kill Project*
+Copyright (C) 2021 Alain Lebret <alain.lebret [at] ensicaen [dot] fr>
+*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+*
+http://www.apache.org/licenses/LICENSE-2.0
+*
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+#include "counter_intelligence.h"
+
+int main(void) 
+{
+    //printf("COUNTER_INTELLIGENCE : pid = %d\n", getpid());
+    memory_t* memory;
+    semaphore_t* sem;
+    sem = open_semaphore("/spy_semaphore");
+    P(sem);
+    memory = get_data();
+    initialize_counter_intelligence(&memory->counter_intelligence, memory);
+    V(sem);
+
+
+}
